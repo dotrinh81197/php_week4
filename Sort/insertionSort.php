d7d14aae9f91c98432805cd0ade040632aeda9c5
dotrinh81197 <dotrinh81197@users.noreply.github.com>
2021-01-04 21:02:16 +0700

    Cài đặt thuật toán sắp xếp chèn

---
+++ Sort/insertionSort.php
@@ -0,0 +1,20 @@
+<?php
+class InsertionSort
+{
+    function insertion_Sort(array $my_array)
+    {
+        for ($i = 0; $i < count($my_array); $i++) {
+            $val = $my_array[$i];
+            $j = $i - 1;
+            while ($j >= 0 && $my_array[$j] < $val) {
+                $my_array[$j + 1] = $my_array[$j];
+                $j--;
+            }
+            $my_array[$j + 1] = $val;
+        }
+        return $my_array;
+    }
+}
+$test_array = [5, -4, 3, 7, 2, 1, 0, 8, 9, 2];
+$insertSort = new InsertionSort();
+print_r($insertSort->insertion_Sort($test_array));
